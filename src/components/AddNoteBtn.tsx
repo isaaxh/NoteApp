cf21b2393429300112b4d6732d9d8ff846dedd81
Isaac Hussain <isaaclearns58@gmail.com>
2023-11-05 04:46:40 +0300

    add note btn with styles

---
+++ src/components/AddNoteBtn.tsx
@@ -0,0 +1,36 @@
+import {StyleSheet, TouchableOpacity} from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/AntDesign';
+
+const AddNoteBtn = () => {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <Icon name="plus" color="#454545" size={30} />
+    </TouchableOpacity>
+  );
+};
+
+export default AddNoteBtn;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ffffff',
+    height: 60,
+    width: 60,
+    borderRadius: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#E5E5E5',
+    position: 'absolute',
+    bottom: 40,
+    right: 20,
+
+    // shadow
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 1},
+    shadowOpacity: 0.8,
+    shadowRadius: 1,
+  },
+});
